1215f3136c1c718acbf4c16ee0f041961ae9d70b
Your Name <you@example.com>
2025-08-13 17:00:07 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,122 @@
+import express from "express"
+import bodyParser from "body-parser";
+import axios from "axios"
+import pg from "pg"
+
+const app = express();
+const port = process.env.PORT ||3000;
+
+const db = new pg.Client({
+    user:"postgres",
+    host:"localhost",
+    database:"booknote",
+    password:"Yogesh@333",
+    port:"5432"
+});
+
+db.connect();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+let books =[{
+    id:3,title:"Atomic Habits",author:"James Clear",
+    rating:5,notes:"nice book",date_read:"2025-08-10",
+}]
+
+app.get("/",async (req,res)=>{
+try{
+const input = await db.query("SELECT * FROM books");
+const books =input.rows
+   books.forEach(book => {
+     book.cover_url = `https://covers.openlibrary.org/b/title/${encodeURIComponent(book.title)}-M.jpg`;
+    }); 
+     res.render("index.ejs", {books});
+}
+
+catch(err){
+    console.log(err)
+}
+});
+
+app.get("/add",(req,res)=>{
+res.render("new.ejs");
+});
+
+app.get("/update",(req,res)=>{
+    res.render("update.ejs");
+});
+
+// async function getCoverUrl(title) {
+//   // Encode title for URL
+//   const encodedTitle = encodeURIComponent(title);
+//   // Open Library cover URL format by title
+//   return `https://covers.openlibrary.org/b/title/${encodedTitle}-M.jpg`;
+// }
+
+
+app.post("/add",async(req,res)=>{
+    try{
+ const { title, author, rating, notes} = req.body;
+//  const cover = await getCoverUrl(title);
+ const date_read = new Date().toISOString().split("T")[0];
+    
+  await db.query(
+    "INSERT INTO books (title, author, rating, notes, date_read) VALUES ($1, $2, $3, $4, $5)",
+    [title, author, rating, notes, date_read]
+    );
+    console.log("result:",{title, author, rating, notes, date_read})
+    res.redirect("/");
+}catch (err) {
+  console.error(err);
+}
+});
+
+// edit 
+app.get("/edit/:id",async(req,res)=> {
+    try{
+    const id = parseInt(req.params.id);
+    console.log("id:",id);
+    const result =await db.query("SELECT * FROM books WHERE id=$1",[id]);
+    const book=result.rows[0];
+    if (!book) return res.status(404).send("Book not found");
+    res.render("edit.ejs",{ book });
+    }
+    catch(err){
+        console.log(err);
+    }
+
+});
+
+// update the books 
+app.post("/edit/:id",async(req,res)=>{
+try{
+const id= parseInt(req.params.id);
+const {title,author, rating, notes, date_read}=req.body;
+await db.query("UPDATE books SET title=$1,author=$2,rating=$3,notes=$4,date_read=$5 WHERE id=$6 RETURNING*",[title,author,rating,notes,date_read,id]);
+
+res.redirect("/");
+}
+catch(err){
+console.log(err)
+}
+});
+
+
+// delete the book
+
+app.post("/delete/:id",async(req,res)=>{
+    try{
+const id= parseInt(req.params.id);
+await db.query("DELETE FROM books WHERE id=$1",[id]);
+res.redirect("/");
+    }
+  catch(err){
+      console.log(err);
+    }
+
+});
+
+app.listen(port, ()=>{
+    console.log(`server running on the port ${port}`);
+});
